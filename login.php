bc9289b526077bcb0f6e8563e0365dad8c9be18d
L3on06 <l3onshabani@gmail.com>
2022-12-28 00:17:00 +0100

    I added all functions like (validation, password hash etc.) to the login and registration page.

--- login.php
+++ login.php
@@ -1,17 +1,61 @@
-<?php
-    $_SESSION['page'] = 'login';
-?>
-
+<?php $_SESSION['page'] = 'Login';?>
 <?php include('Components/Header.php')?>
 
+<?php 
+    include('classes/CRUD.php');
+    $crud = new CRUD;
+
+    $error = '';
+    $errors = [];
+
+if(isset($_POST['login_btn'])){       
+        //validation
+         if(strlen($_POST['username']) < 3)
+            $errors[] = 'username is empty or too short!';
+
+        if(strlen($_POST['password']) < 8)
+            $errors[] = 'Password is not valid!';
+        
+        $user = $crud->read('users', ['column' => 'username', 'value' => $_POST['username']], 1);
+
+        if((count($errors) === 0) && is_array($user) && (count($user) > 0)){
+          $user = $user[0];
+        }
+
+        if(password_verify($_POST['password'], $user['password'])){
+            //set session
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['is_loggedin'] = true;
+
+            //redirect
+            header('Location: /eBook/dashboard/index.php');
+        } else {
+        $_SESSION['error'] = 'Credentials are not correct!';
+        }
+    } else {
+        $_SESSION['error'] = 'User does not exist!';
+    }
+?>
 
 <div class="auth my-5">
     <div class="container">
-        <h2 class="text-center mb-4">Login</h2>
-        <form>
+        <h2 class="text-center mb-4">Login</h2>   
+        <?php if(isset($_SESSION['error'])) echo '<p>'.$_SESSION['error'].'</p>'; ?>
+        <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
+                <?php 
+                    if(count($errors)) {
+                        echo '<ul>';
+                        foreach($errors as $error) {
+                            echo '<li>'.$error.'</li>';
+                        }
+                        echo '</ul>';
+                    }
+                ?>
+        <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
             <div class="mb-3">
-                <label for="username" class="form-label">Email address</label>
-                <input type="email" name="username" class="form-control" id="username" aria-describedby="usernameHelp" />
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" class="form-control" id="username" aria-describedby="usernameHelp" />
                 <div id="usernameHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
@@ -22,7 +66,7 @@
                 <input type="checkbox" class="form-check-input" id="remberme" value="yes">
                 <label class="form-check-label" for="remmberme">Remember me</label>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-primary" name="login_btn">Login</button>
             <a href="register.php" class="btn btn-link">Let me register first</a>
         </form>
     </div>
--- register.php
+++ register.php
@@ -1,13 +1,69 @@
-<?php
-    $_SESSION['page'] = 'register';
-?>
+<?php$_SESSION['page'] = 'register';?>
 <?php include('Components/Header.php')?>
 
+<?php 
+    include('classes/CRUD.php');
+    $crud = new CRUD;
+
+    $error = '';
+    $errors = [];
+
+if(isset($_POST['register_btn'])){       
+        //validation
+         if(strlen($_POST['username']) < 3)
+            $errors[] = 'username is empty or too short!';
+
+        if(strlen($_POST['email']) < 4)
+            $errors[] = 'email is not valid!';
+
+        if(strlen($_POST['address']) < 4)
+            $errors[] = 'address is not valid!';
+            
+        if(strlen($_POST['phone']) < 6)
+            $errors[] = 'phone is not valid!';
+
+        if(strlen($_POST['password1']) < 8)
+            $errors[] = 'Password is not valid!';
+
+        if(strlen($_POST['password2']) < 8)
+            $errors[] = 'Repeat password is not valid!';
+
+        if($_POST['password1'] !== $_POST['password2'] )
+            $errors[] = 'Password has to be same as Reapeat password!';
+    
+        
+        if(count($errors) === 0){
+            $password = password_hash($_POST['password1'], PASSWORD_BCRYPT);
+
+             if($crud->create('users', [
+                'username' => $_POST['username'],
+                'email' => $_POST['email'],
+                'address' => $_POST['address'],
+                'phone' => $_POST['phone'],
+                'password' => $password,
+            ])) {
+                header('Location: login.php');
+            } else {
+                $error = 'Something want wrong!';
+            }
+        }
+    } 
+?>
 
 <div class="auth my-5">
     <div class="container">
         <h2 class="text-center mb-4">Register</h2>
-        <form>
+        <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
+                <?php 
+                    if(count($errors)) {
+                        echo '<ul>';
+                        foreach($errors as $error) {
+                            echo '<li>'.$error.'</li>';
+                        }
+                        echo '</ul>';
+                    }
+                ?>
+        <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
                 <input type="text" name="username" class="form-control" id="username" require/>
@@ -33,7 +89,7 @@
                 <label for="password2" class="form-label">Repeat Password</label>
                 <input type="password" name="password2" class="form-control" id="password2" require/>
             </div>
-            <button type="submit" class="btn btn-primary">Register</button>
+            <button type="submit" class="btn btn-primary" name="register_btn">Register</button>
             <a href="login.php" class="btn btn-link">I already have an account</a>
         </form>
     </div>
