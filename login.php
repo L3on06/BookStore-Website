bdade4fc0827510a68da8017b9da30c3ab818f9e
L3on06 <l3onshabani@gmail.com>
2023-01-09 07:03:34 +0100

    I redesigned the login and registration page.

--- login.php
+++ login.php
@@ -38,38 +38,72 @@ if(isset($_POST['login_btn'])){
     }
 ?>
 
-<div class="auth my-5">
-    <div class="container">
-        <h2 class="text-center mb-4">Login</h2>   
-        <?php if(isset($_SESSION['error'])) echo '<p>'.$_SESSION['error'].'</p>'; ?>
-        <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
-                <?php 
-                    if(count($errors)) {
-                        echo '<ul>';
+<section class="vh-100 " style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row flex-row-reverse justify-content-center">
+         <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
+                    <?php 
+                        if(count($errors)) {
+                          echo '<ul>';
                         foreach($errors as $error) {
                             echo '<li>'.$error.'</li>';
                         }
-                        echo '</ul>';
+                            echo '</ul>';
                     }
                 ?>
-        <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" name="username" class="form-control" id="username" aria-describedby="usernameHelp" />
-                <div id="usernameHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <img src="./assets/img/logIn.png" class="img-fluid" alt="Sample image">
+
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Log In</p>
+
+                 <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                         <label for="username" class="form-label">Username</label>
+                        <input type="text" name="username" class="form-control" id="username" aria-describedby="usernameHelp" required/>
+                    </div>
+                  </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control" id="password"  required/>
+                      </div>
+                    </div>
+                    
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <input type="checkbox" name="rememberMe" value="1" class="form-check-input" id="rememberMe" value="yes">
+                    <label class="form-check-label" for="rememberMe">Remember me</label>
+                  </div>
+
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <button type="submit" class="btn btn-primary" name="login_btn">Login</button>
+                     <a href="register.php" class="btn btn-link">Let me register first</a>
+                  </div>
+
+
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" id="password" />
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="remberme" value="yes">
-                <label class="form-check-label" for="remmberme">Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-primary" name="login_btn">Login</button>
-            <a href="register.php" class="btn btn-link">Let me register first</a>
-        </form>
+            
+        </div>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
+</section>
 
 <?php include('Components/footer.php')?>
+
+
+
--- register.php
+++ register.php
@@ -10,7 +10,7 @@
 
 if(isset($_POST['register_btn'])){       
         //validation
-         if(strlen($_POST['username']) < 3)
+        if(strlen($_POST['username']) < 3)
             $errors[] = 'username is empty or too short!';
 
         if(strlen($_POST['email']) < 4)
@@ -50,10 +50,78 @@ if(isset($_POST['register_btn'])){
     } 
 ?>
 
-<div class="auth my-5">
-    <div class="container">
-        <h2 class="text-center mb-4">Register</h2>
-        <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
+
+<section class="auto pt-5 pb-5" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                 <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" name="username" class="form-control" id="username" required/>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                       <label for="email" class="form-label">Email</label>
+                         <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" required/>
+                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                  </div>
+
+                  
+                  <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                          <label for="address" class="form-label">Address</label>
+                          <input type="text" name="address" class="form-control" id="address" required/>
+                        </div>
+                    </div>
+        
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                       <label for="phone" class="form-label">Phone</label>
+                          <input type="phone" name="phone" class="form-control" id="phone" required/>
+                      </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password1" class="form-label">Password</label>
+                        <input type="password" name="password1" class="form-control" id="password1" required/>
+                      </div>
+                    </div>
+                    
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password2" class="form-label">Repeat Password</label>
+                        <input type="password" name="password2" class="form-control" id="password2" required/>
+                      </div>
+                    </div>
+                    
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <button type="submit" class="btn btn-primary" name="register_btn">Register</button>
+                    <a href="login.php" class="btn btn-link">I already have an account</a>
+                  </div>
+
+                </form>
+              </div>
+              <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
                 <?php 
                     if(count($errors)) {
                         echo '<ul>';
@@ -63,36 +131,19 @@ if(isset($_POST['register_btn'])){
                         echo '</ul>';
                     }
                 ?>
-        <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" name="username" class="form-control" id="username" require/>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="./assets/img/SignUp.jpg" class="img-fluid" alt="Sample image">
+
+              </div>
             </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" require/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-              <div class="mb-3">
-                <label for="address" class="form-label">Address</label>
-                <input type="text" name="address" class="form-control" id="address" require/>
-            </div>
-              <div class="mb-3">
-                <label for="phone" class="form-label">Phone</label>
-                <input type="phone" name="phone" class="form-control" id="phone" require/>
-            </div>
-            <div class="mb-3">
-                <label for="password1" class="form-label">Password</label>
-                <input type="password" name="password1" class="form-control" id="password1" require/>
-            </div>
-            <div class="mb-3">
-                <label for="password2" class="form-label">Repeat Password</label>
-                <input type="password" name="password2" class="form-control" id="password2" require/>
-            </div>
-            <button type="submit" class="btn btn-primary" name="register_btn">Register</button>
-            <a href="login.php" class="btn btn-link">I already have an account</a>
-        </form>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
+</section>
 
 <?php include('Components/footer.php')?>
+
+
