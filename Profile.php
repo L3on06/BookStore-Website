9bde51e8b17ebe6011f506aa44ba0e555bff6b28
L3on06 <l3onshabani@gmail.com>
2023-01-12 08:32:35 +0100

    I redesigned the Profile page.

--- Profile.php
+++ Profile.php
@@ -85,77 +85,100 @@
     }
 ?>
 
-<div class="profile my-5">
-    <div class="container">
-        <h3 class="mb-4">Profile l3on06</h3>
-        <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
-        <?php 
-            if(count($errors)) {
-                echo '<ul>';
-                foreach($errors as $error) {
-                    echo '<li>'.$error.'</li>';
-                }
-                echo '</ul>';
-            }
-        ?>
-        <?php 
-        if(isset($_GET['status'])) {
-            switch($_GET['status']) {
-                case 'success':
-                    echo 'Action was performed successfully';
-                    break;
-                case 'error':
-                    echo 'Something want wrong!';
-                    break;
-            }
-        }
-        ?>
-        <div class="card mb-4">
-            <div class="card-header">
-                <h5>Update</h5>
+<section class="auto pt-5 pb-5" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Profile <?= $user['username'] ?></p>
+
+                 <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" name="username" class="form-control" id="username"  required <?php if(!is_null($user)):?> value='<?= $user['username'] ?>' <?php endif; ?>/>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                          <label for="email" class="form-label">Email</label>
+                          <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" required <?php if(!is_null($user)):?> value='<?= $user['email'] ?>' <?php endif; ?>/>
+                          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                  </div>
+
+                  
+                  <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                            <label for="address" class="form-label">Address</label>
+                            <input type="text" name="address" class="form-control" id="address"  required <?php if(!is_null($user) && !empty($user['address'])):?> value='<?= $user['address'] ?>' <?php endif; ?>/>
+                        </div>
+                    </div>
+        
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                            <label for="phone" class="form-label">Phone</label>
+                            <input type="phone" name="phone" class="form-control" id="phone"  required <?php if(!is_null($user) && !empty($user['phone']) ):?> value='<?= $user['phone'] ?>' <?php endif; ?>/>
+                      </div>
+                    </div>
+
+                    <div class="form-check d-flex justify-content-end mb-5">
+                        <button type="submit" class="btn btn-primary" name="update_profile_btn">Update informations</button>
+                    </div>
+                    
+                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Password</p>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password1" class="form-label">Password</label>
+                        <input type="password" name="password1" class="form-control" id="password1" />
+                      </div>
+                    </div>
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password2" class="form-label">Repeat Password</label>
+                        <input type="password" name="password2" class="form-control" id="password2"/>
+                      </div>
+                    </div>
+                  <div class="form-check d-flex justify-content-end mb-5">
+                   <button type="submit" class="btn btn-primary" name="update_password_btn">Update Password</button>
+                  </div>
+
+                </form>
+              </div>
+              <?php if(isset($error)) echo '<p>'.$error.'</p>'; ?>
+                <?php 
+                    if(count($errors)) {
+                        echo '<ul>';
+                        foreach($errors as $error) {
+                            echo '<li>'.$error.'</li>';
+                        }
+                        echo '</ul>';
+                    }
+                ?>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="./assets/img/SignUp.jpg" class="img-fluid" alt="Sample image">
+
+              </div>
             </div>
-            <div class="card-body">
-        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" name="username" class="form-control" id="username"  required <?php if(!is_null($user)):?> value='<?= $user['username'] ?>' <?php endif; ?>/>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" required <?php if(!is_null($user)):?> value='<?= $user['email'] ?>' <?php endif; ?>/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-              <div class="mb-3">
-                <label for="address" class="form-label">Address</label>
-                <input type="text" name="address" class="form-control" id="address"  required <?php if(!is_null($user) && !empty($user['address'])):?> value='<?= $user['address'] ?>' <?php endif; ?>/>
-            </div>
-              <div class="mb-3">
-                <label for="phone" class="form-label">Phone</label>
-                <input type="phone" name="phone" class="form-control" id="phone"  required <?php if(!is_null($user) && !empty($user['phone']) ):?> value='<?= $user['phone'] ?>' <?php endif; ?>/>
-            </div>
-            <button type="submit" class="btn btn-primary" name="update_profile_btn">Update</button>
-        </form>
-            </div>
+          </div>
         </div>
-         <div class="card mb-4">
-            <div class="card-header">
-                <h5>Change Password</h5>
-            </div>
-            <div class="card-body">
-        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-            <div class="mb-3">
-                <label for="password1" class="form-label">Password</label>
-                <input type="password" name="password1" class="form-control" id="password1"  require/>
-            </div>
-            <div class="mb-3">
-                <label for="password2" class="form-label">Repeat Password</label>
-                <input type="password" name="password2" class="form-control" id="password2"  require/>
-            </div>
-            <button type="submit" class="btn btn-primary" name="update_password_btn">Update</button>
-        </form>
-            </div>
-        </div>
+      </div>
     </div>
-</div>
+  </div>
+</section>
 
 <?php include('Components/Footer.php'); ?>
