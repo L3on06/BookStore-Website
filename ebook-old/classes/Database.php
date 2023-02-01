99cdb42186593aed1347435d9bbc423563f482e6
L3on06 <l3onshabani@gmail.com>
2023-02-01 22:45:40 +0100

    Converting to laravel.

--- ebook-old/classes/Database.php
+++ ebook-old/classes/Database.php
@@ -4,9 +4,15 @@ class Database {
     private static $instance = null;
     private $connection = null;
 
-    public function __construct() {
-        $this->connection = new mysqli('localhost', 'root', '', 'ebook');
-    }
+    // public function __construct() {
+    //     $this->connection = new mysqli('localhost', 'root', '', 'ebook');
+    // }
+
+public function __construct() { $this->connection = new mysqli( 
+    'localhost', 
+    'id20199999_ebook', 
+    'Tii)8ntr)ii86wGV', 
+    'id20199999_admin'); }
 
     public static function getInstance() {
         if(!self::$instance) {
