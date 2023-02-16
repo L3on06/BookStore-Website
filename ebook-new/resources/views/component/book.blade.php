5c59bdea36844548edee48ecb59244a562c33f53
L3on06 <l3onshabani@gmail.com>
2023-02-16 20:44:02 +0100

    I added a new style for the shopping and book viewing sections, optimized for all screen sizes.

--- ebook-new/resources/views/component/book.blade.php
+++ ebook-new/resources/views/component/book.blade.php
@@ -2,35 +2,45 @@
     {{$book->title}}
     @endsection
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <div class="swiper-slide drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.6)] hover:drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.8)]">
-            <img src="{{asset('storage/books/' .$book->image )}}" class="h-72 vhover:scale-[1.05] mx-4"  alt="{{$book->title}}" />
-        </div>
-    </a>
-    <div class="px-5 pb-5">
-        <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{$book->title}}</h5>
-        </a>
-        <div class="flex items-center mt-2.5 mb-5">
-            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
-        </div>
-        <div class="flex items-center justify-between">
-            <div>
-                <span>In stock {{$book->qty}}</span>
-                <span class="text-3xl font-bold text-gray-900 dark:text-white">${{$book->price}}</span>
-            </div>
-        </div>
+{{-- grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6
+    border border-2 rounded-3xl shadow-xl
+    --}}
 
-        <form action="{{route('cart.add', ['book' => $book->id])}}" method="POST">
-        @csrf
-        <input type="number" value="1" name="qty" id="qty" min="1" max="{{$book->qty}}" />
-        <button type="submit" class="">Add to cart</button>
-        </form>
+
+<div class="container mx-auto my-44">
+    <div class="w-full grid grid-cols-1 lg:grid-cols-2">
+        <div class="drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.6)] hover:drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.8)]">
+            <img  src="{{asset('storage/books/' .$book->image )}}" class="rounded-2xl p-2 mx-auto w-[23rem] h-[35rem] object-cover" alt="{{$book->title}}">
+        </div>
+     <div class="px-5 pb-5">
+            <h5 class="font-serif text-5xl mt-10">{{$book->title}}</h5>
+
+            <form action="{{route('cart.add', ['book' => $book->id])}}" method="POST">
+                @csrf
+                <div class="mt-10">
+                    <div class="flex justify-between">
+                        <input class="p-2 text-center border border-4 border-[#e6ddc4]" type="number" value="1" name="qty" id="qty" min="1" max="{{$book->qty}}"/>
+                        <span class="font-bold text-2xl p-2 border border-4 border-[#e6ddc4]">In stock: {{$book->qty}}</span>
+                    </div>
+                    <div class="my-6 flex justify-between">
+                        <span class="text-5xl font-bold text-gray-900">{{number_format($book->price, 2, '.', '')}} &euro;</span>
+                        <button type="submit" class="w-1/2 py-4  text-[#6d4b2f] font-semibold bg-[#e6ddc4] hover:text-[#e6ddc4] hover:bg-[#6d4b2f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center">Add to cart</button>
+                    </div>
+                </div>
+                </form>
+
+                <div class="w-full col-span-2 mt-10 shadow-lg p-5 ">
+                    <h1 class="text-4xl font-serif">Description</h1>
+
+                    <p class="py-5 text-lg ">{{$book->description}}</p>
+                  </div>
+
+            </div>
+
+
+
+
+
+
     </div>
 </div>
