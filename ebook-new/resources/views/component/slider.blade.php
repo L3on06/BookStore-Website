ba5bc2c6b58c41d637b730da5f1211638f17d253
L3on06 <l3onshabani@gmail.com>
2023-02-19 18:40:30 +0100

    I implemented a function to display a dynamic sliders.

--- ebook-new/resources/views/component/slider.blade.php
+++ ebook-new/resources/views/component/slider.blade.php
@@ -1,40 +1,39 @@
-<div class="container my-0 mx-auto">
-    <h2 class="text-center my-5 text-4xl font-bold p-5 tracking-wide slider-title"><span>Best books of the year</span></h2>
-    <hr  class="">
-      <div class="swiper">
-          <div class="swiper-wrapper">
+@php $matchFound = true; @endphp
+<div class="container my-10 mx-auto">
+    @if ($sliders && count($sliders) > 0)
+    @foreach ($sliders as $slider)
+    @foreach ($books as $book)
+              @if($book->slider_category_id === $slider->id && $matchFound)
+    <h2 class="text-center my-5 text-4xl font-bold p-5 tracking-wide slider-title"><span>{{$slider->name}}</span></h2>
+    <hr>
+    <div class="swiper">
+        <div class="swiper-wrapper">
 
-@if ($books && count($books) > 0)
-@foreach ($books as $book)
+            {{-- Look if we have book at this category --}}
+        @foreach ($books as $book)
+            @if($book->slider_category_id === $slider->id)
 
+              {{-- Books Slider --}}
+                <div class="swiper-slide drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.6)] hover:drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.8)]">
+                    <a href="{{route('books.show', ['book' => $book->id]) }}">
+                        <img  src="{{asset('storage/books/' .$book->image )}}" class="h-72 vhover:scale-[1.05] mx-4"  alt="{{$book->title}}">
+                    </a>
+                </div>
 
+            @endif
+        @endforeach
 
-
-<!-- Slides -->
-<div class="swiper-slide drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.6)] hover:drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.8)]">
-    <a href="{{route('books.show', ['book' => $book->id]) }}">
-        <img  src="{{asset('storage/books/' .$book->image )}}" class="h-72 vhover:scale-[1.05] mx-4"  alt="{{$book->title}}">
-    </a>
 </div>
-{{-- <div class="swiper-slide drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.6)] hover:drop-shadow-[-10px_6px_12px_rgba(0,0,0,0.8)]">
-<a href="ViewBook.php?id=">
-<img src="{{asset('/images/books/book37.jpg')}}" class="h-72 hover:scale-[1.05] mx-4"  alt="Best books of the year">
-</a>
-</div> --}}
+<div class="swiper-button-prev"></div>
+<div class="swiper-button-next"></div>
+<div class="swiper-pagination"></div>
+</div>
+<hr>
+@php
+    break;
+    @endphp
+    @endif
 @endforeach
-
-@else
-
-<h1>
-    no book
-</h1>
-
+@endforeach
 @endif
-            </div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-pagination"></div>
-            </div>
-            <hr class="">
-        </div>
 </div>
